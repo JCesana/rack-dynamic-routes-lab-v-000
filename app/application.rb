0ea28799787b4e45c87169a1f4645273a8e09136
Josh Cesana <joshuacesana@gmail.com>
2018-07-22 20:06:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,20 +14,12 @@ class Application
         resp.write "Item not found"
         resp.status = 400  
       else 
-        resp
+        resp.write item.price
       end 
-      resp.write "#{Item.price}"
+      
     else
       resp.write "Route not found"
       resp.status = 404
-    end
-    
-    if req.path.match(/songs/)
- 
-      item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
-      song = @@songs.find{|s| s.title == song_title}
- 
-      resp.write song.artist
     end
     
     resp.finish
