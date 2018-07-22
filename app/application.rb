0c32fcbd7014c47e4e7d2321070f1f27de09f363
Josh Cesana <joshuacesana@gmail.com>
2018-07-22 19:57:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,11 +4,19 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
-    if req.path == "/items/#{Item}"
+    if req.path.match(/items/)
       resp.write "#{Item.price}"
     else
       resp.write "Route not found"
       resp.status = 404
+    end
+    
+    if req.path.match(/songs/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
     end
     
     resp.finish
